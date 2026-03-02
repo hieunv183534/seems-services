5e5a58e1b1d0813490d1388581cf4c3edc9cfc6a
unknown <vhieukk20000@gmail.com>
2026-03-02 21:39:10 +0700

    refactor seperate sso server

---
+++ apps/VNEms.AuthServer/VNEmsBrandingProvider.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Localization;
+using VNEmsAdmin.Localization;
+using Volo.Abp.Ui.Branding;
+using Volo.Abp.DependencyInjection;
+
+namespace VNEmsAdmin;
+
+[Dependency(ReplaceServices = true)]
+public class VNEmsBrandingProvider : DefaultBrandingProvider
+{
+    private IStringLocalizer<VNEmsAdminResource> _localizer;
+
+    public VNEmsBrandingProvider(IStringLocalizer<VNEmsAdminResource> localizer)
+    {
+        _localizer = localizer;
+    }
+
+    public override string AppName => _localizer["AppName"];
+}
